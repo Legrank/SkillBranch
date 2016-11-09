4bfac760077770970bd9b85a92dac3abb689211a
Dima <flyu.gyu@yandex.ru>
2016-11-09 17:59:58 +0300

    format

--- skb2a/index.js
+++ skb2a/index.js
@@ -4,21 +4,22 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 app.get('/fullname', (req, res) => {
-    function fullname(){
-       const myRe = /\S+/g;
-       if (!req.query.fullname || /[\d_/\\]/.test(req.query.fullname)) {
-         return "Invalid fullname";
-       }
-       const nameArray = req.query.fullname.match(myRe);
-       if ((nameArray == null) || (nameArray.length>3)) {
-         return "Invalid fullname";
-       }
-       let fullname = nameArray.pop();
-       fullname = fullname[0].toUpperCase()+fullname.slice(1).toLowerCase();
-       for (let i = 0; i < nameArray.length; i++) {
-         fullname += " "+nameArray[i][0].toUpperCase()+'.';
-       }
-       return fullname;
+function fullname(){
+        const query = req.query.fullname;
+        if (!query || /[\d_/]/.test(query)) {
+            return "Invalid fullname";
+        }
+        const myRe = /\S+/g;
+        const nameArray = query.match(myRe);
+        if ((nameArray == null) || (nameArray.length>3)) {
+            return "Invalid fullname";
+        }
+        let fullname = nameArray.pop();
+        fullname = fullname[0].toUpperCase()+fullname.slice(1).toLowerCase();
+        for (let i = 0; i < nameArray.length; i++) {
+            fullname += ` ${nameArray[i][0].toUpperCase()}.`;
+        }
+        return fullname;
     }
     res.send(fullname());
 });
@@ -28,5 +29,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Your app listening on port 3000!');
+    console.log('Your app listening on port 3000!');
 });
